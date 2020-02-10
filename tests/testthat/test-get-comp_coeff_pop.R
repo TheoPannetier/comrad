72a9ccf0b791def09beea0a3ea17e3331e386eec
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-02-10 18:45:01 +0100

    Revamped tests along with the faster implementation of `get_comp_coeff_pop()`. Should provide 100% codecov but argument format are not covered. E.g. if any trait is set to Inf or competition width to <0, things will go awry.

--- tests/testthat/test-get-comp_coeff_pop.R
+++ tests/testthat/test-get-comp_coeff_pop.R
@@ -1,48 +1,88 @@
 context("test-get_comp_coeff_pop")
 
-test_pop <- c(-0.39, 0.25, 1.3)
+is_on_ci <- (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "")
+nb_random_pops <- ifelse(is_on_ci, 20, 1)
 
-test_that("use", {
-  # Ordinary cases
+test_that("regular_cases", {
+  # Calibrated result
   expect_equal(
-    get_comp_coeff_pop(0.5, 0.7, sqrt(0.2)),
-    0.9048374,
+    get_comp_coeff_pop(
+      trait_ind = 0,
+      traits_pop = seq(-1, 0, by = 0.25),
+      comp_width = 0.2
+    ),
+    c(3.726653e-06, 8.838263e-04, 4.393693e-02, 4.578334e-01, 1),
     tolerance = 1e-07
-    )
-  expect_equal(get_comp_coeff_pop(0.5, 0.5, 0.2), 1) # same trait case
-  expect_equal(get_comp_coeff_pop(-5, 5, 0.2), 0) # distant trait case
+  )
+
+  # All individuals identical
   expect_equal(
-    get_comp_coeff_pop(5, -1, 0.2),
-    get_comp_coeff_pop(-5, 1, 0.2)
-    ) # symmetry
-  expect_equal(get_comp_coeff_pop(3.5, -0.39, sqrt(1.2)), 0.00182707)
-  # Border cases of trait_dist and sigma_comp
-  expect_equal(get_comp_coeff_pop(0, rep(0, 5), 0), rep(1, 5))
-  expect_equal(get_comp_coeff_pop(0, rep(0, 5), Inf), rep(1, 5))
-  expect_equal(get_comp_coeff_pop(Inf, rep(0, 5), 0), rep(0, 5))
-  expect_equal(get_comp_coeff_pop(Inf, rep(0, 5), Inf), rep(1, 5))
+    get_comp_coeff_pop(trait_ind = 0, traits_pop = rep(0, 3)),
+    rep(1, 3)
+  )
+  # All individuals identical and infinite competition width
+  expect_equal(
+    get_comp_coeff_pop(
+      trait_ind = 0,
+      traits_pop = rep(0, 5),
+      comp_width = Inf
+    ),
+    rep(1, 5)
+  )
+
+  # All individuals identical and zero-width competition
+  expect_equal(
+    get_comp_coeff_pop(
+      trait_ind = 0,
+      traits_pop = rep(0, 5),
+      comp_width = 0
+    ),
+    rep(0, 5)
+  )
+})
+
+test_that("random_pops", {
+  for (i in nb_random_pops) {
+    random_pop <- rnorm(5, 0, 0.1)
+    random_sd <- runif(1, 0, 1)
+
+    # Regular random case
+    random_coeffs <- get_comp_coeff_pop(
+      trait_ind = random_pop[1],
+      traits_pop =  random_pop,
+      comp_width = random_sd
+    )
+    expect_silent(testarg_num(random_coeffs))
+    expect_silent(testarg_prop(random_coeffs))
+
+    # Zero-width competition width
+    expect_equal(
+      get_comp_coeff_pop(
+        trait_ind = random_pop[1],
+        traits_pop =  random_pop,
+        comp_width = 0
+      ),
+      rep(0, 5)
+    )
+    # Infinite competition width
+    expect_equal(
+      get_comp_coeff_pop(
+        trait_ind = random_pop[1],
+        traits_pop = random_pop,
+        comp_width = Inf
+      ),
+      rep(1, 5)
+    )
+  }
 })
 
 test_that("abuse", {
+  # Testing that values in the arguments are of the correct format takes
+  # too much on the running time. I  have left only the most important and trust
+  # higher function calls to control the right format of parameters.
 
   expect_error(
-    object = get_comp_coeff_pop(TRUE, -0.3, 0.2),
-    regexp = "'trait_ind' must be numeric"
-  )
-  expect_error(
-    object = get_comp_coeff_pop(0.5, -0.3, NaN),
-    regexp = "'comp_width' contains one or more NaNs"
-  )
-  expect_error(
-    object = get_comp_coeff_pop(0.5, "my_old_man", 0.2),
-    regexp = "'traits_pop' must be numeric"
-  )
-  expect_error(
-    object = get_comp_coeff_pop(0.5, 0.1, "this_old_dog"),
-    regexp = "'comp_width' must be numeric"
-  )
-  expect_error(
-    object = get_comp_coeff_pop(0.5, 0.7, -1),
-    regexp = "'comp_width' must be a positive numeric"
+    get_comp_coeff_pop(trait_ind = c(0, 0), traits_pop = c(0, 0)),
+    "'trait_ind' must have length 1"
   )
 })
