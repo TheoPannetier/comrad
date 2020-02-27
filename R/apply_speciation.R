c2c43e39876ee965b181328575f6f7a04c7f0a63
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-02-27 11:15:24 +0000

    Modify speciation: species are no longer randomly split, instead the smaller half (nb of indinviduals) becomes a new species.

--- R/apply_speciation.R
+++ R/apply_speciation.R
@@ -1,7 +1,8 @@
 #' Resolve speciation in a comrad community
 #'
 #' For each species, check if there is any gap of `>=` 0.1 in trait values, and
-#' split the relevant species in two when one is found.
+#' split the relevant species in two when one is found. The less numerous half
+#' becomes the new species.
 #'
 #' @inheritParams default_params_doc
 #'
@@ -32,6 +33,7 @@ apply_speciation <- function(comm) {
     where <- which(comm$species == sp)
     # Extract members of focal species
     sp_members <- comm %>% dplyr::filter(species == sp)
+    nb_inds <- length(sp_members$z)
 
     # Check for gaps in trait values -------------------------------------------
     gaps <- sp_members %>%
@@ -40,13 +42,8 @@ apply_speciation <- function(comm) {
       find_trait_gaps()
 
     if (length(gaps) > 0) {
-      gap <- gaps[1] # only the first gap is treated (soft polytomy)
+      gap <- gaps[1] # only the first gap is treated  for now (soft polytomy)
       # Split species in two ---------------------------------------------------
-
-      # Flip a coin to determine which side of the gap becomes the new species
-      coin_flip <- stats::rbinom(n = 1, size = 1, prob = 0.5)
-
-      # for (i in seq_along(gaps)) {
       new_sp <- charlatan::ch_hex_color()
       sp_labels <- sp_members %>%
         dplyr::select(species) %>%
@@ -54,15 +51,26 @@ apply_speciation <- function(comm) {
       anc_labels <- sp_members %>%
         dplyr::select(ancestral_species) %>%
         unlist()
-      if (coin_flip == 1) {
+
+      # Less numerous becomes new species
+      if (gap < nb_inds / 2) {
         sp_labels[1:gap] <- new_sp
         anc_labels[1:gap] <- sp
-      } else {
+      } else if (gap > nb_inds / 2) {
         sp_labels[(gap + 1):length(sp_labels)] <- new_sp
         anc_labels[(gap + 1):length(anc_labels)] <- sp
-
+      } else { # same length, split at random
+        # Flip a coin to determine which side of the gap becomes the new species
+        coin_flip <- stats::rbinom(n = 1, size = 1, prob = 0.5)
+        if (coin_flip == 1) {
+          sp_labels[1:gap] <- new_sp
+          anc_labels[1:gap] <- sp
+        } else {
+          sp_labels[(gap + 1):length(sp_labels)] <- new_sp
+          anc_labels[(gap + 1):length(anc_labels)] <- sp
+        }
       }
-      # }
+
       # Test format & update community ---------------------------------
       comrad::testarg_char(sp_labels)
       comrad::testarg_length(sp_labels, length(sp_members$species))
