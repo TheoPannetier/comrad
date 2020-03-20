f326b98a043ee008f6e95ff142b8d39801e3fc54
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-03-20 00:01:14 +0000

    diversity-dependent rates reconstruction

---
+++ R/estimate_dd_rates.R
@@ -0,0 +1,80 @@
+#' Estimate diversity-dependent rates from phylogenies
+#'
+#' For a list of phylogenies, reconstruct the *average, per-capita* rates of
+#' speciation and extinction corresponding to each level of species diversity.
+#'
+#' @param multi_phylo a list of `phylo` objects, as typically created with
+#' `ape`.
+#'
+#' @return a `tibble` with a row for every possible number of species and 4
+#' columns:
+#'
+#' * `N`, level of diversity, nb of species
+#' * `nb_events`, the number of replicates for that level of diversity; that is,
+#' the number of events corresponding to that state of diversity, across all
+#' phylogenies in `multi_phylo`.
+#' * `speciation_rate` the reconstructed, average, per-capita speciation rate
+#' for that level of diversity
+#' * `extinction_rate` the reconstructed, average, per-capita extinction rate
+#' for that level of diversity
+#'
+#' @return
+#' @author Théo Pannetier
+#' @export
+#'
+
+estimate_dd_rates <- function(multi_phylo) {
+
+  if (!is.list(multi_phylo)) {
+    stop("'multi_phylo' must be a list of 'phylo' objects.")
+  } else if (!all(ape::is.binary.multiPhylo(multi_phylo))) {
+    stop("'multi_phylo' must be a list of 'phylo' objects.")
+  }
+
+  # Declare empty variables for check
+  N <- NULL
+  time <- NULL
+  event_rate <- NULL
+  waiting_time <- NULL
+  prop_speciation <- NULL
+  prop_extinction <- NULL
+  speciation_rate <- NULL
+  extinction_rate <- NULL
+  mean_waiting_time <- NULL
+  nb_events <- NULL
+  event <- NULL
+  total_events <- NULL
+
+  replicates <- seq_along(multi_phylo)
+
+  # Extract waiting times for all phylos
+  times_tbl <- lapply(replicates, function (i) {
+    phylo <- multi_phylo[[i]]
+    waiting_times(phylo) %>%
+      dplyr::mutate("replicate" = i)
+  }) %>%
+    dplyr::bind_rows()
+
+  # Get nb of events (speciation + extinction) for each N
+  sample_sizes <- times_tbl %>%
+    dplyr::group_by(N) %>%
+    dplyr::summarise(
+      "total_events" = dplyr::n()
+    )
+  # Average wating times
+  rates_tbl <- times_tbl %>%
+    dplyr::group_by(N, event) %>%
+    dplyr::summarise(
+      "mean_waiting_time" = mean(waiting_time, na.rm = TRUE),
+      "nb_events" = dplyr::n()
+    )
+  # Compute speciation & extinction rates
+  rates_tbl <- rates_tbl %>%
+    dplyr::bind_cols(sample_sizes[rates_tbl$N, "total_events"]) %>%
+    dplyr::mutate(
+      "event_rate" = (1 / mean_waiting_time) * (1 / N) *
+        (nb_events / total_events)
+    )
+  rates_tbl
+}
+
---
+++ R/waiting_times.R
@@ -0,0 +1,54 @@
+#' Extract times to events from a phylogeny
+#'
+#' Turns the phylogeny into a lineage-through-time table, and for which
+#' event computes the time to next event.
+#'
+#' @param phylo a `phylo` object, as created by `ape` functions
+#'
+#' @return a `tibble` with a row per event and four columns:
+#'
+#' * `time`, time of an event
+#' * `N`, number of species in the tree at that time
+#' * `waiting_time`, time to the next event
+#' * `next_event`, type of the next event; either `"speciation"` or
+#' `"extinction"`
+#'
+#' @author Théo Pannetier
+#' @export
+
+waiting_times <- function(phylo) {
+  if (!ape::is.binary.phylo(phylo)) {
+    stop("'phylo' must be a 'phylo' object.")
+  }
+
+  time <- NULL
+
+  # Get time and N from ape
+  ltt_tbl <- phylo %>% ape::ltt.plot.coords() %>%
+    tibble::as_tibble()
+
+  ltt_tbl <- ltt_tbl %>% dplyr::mutate(
+    waiting_time = time - dplyr::lag(time)
+  ) %>%
+    dplyr::mutate(
+      # ape misplaces time relative to N, fix that
+      time = dplyr::lag(time - min(time))
+    )
+  # first row is useless
+  ltt_tbl <- ltt_tbl[-1, ]
+
+  # Time to speciation or extinction ?
+  ltt_tbl <- ltt_tbl %>%
+    dplyr::mutate(
+      "event" = ifelse(
+        test = dplyr::lead(ltt_tbl$N) - ltt_tbl$N > 0,
+        yes = "speciation",
+        no = "extinction"
+      )
+    )
+  # exclude last row, waiting time is cut by present
+  ltt_tbl <- ltt_tbl[-nrow(ltt_tbl), ]
+
+
+  ltt_tbl
+}
