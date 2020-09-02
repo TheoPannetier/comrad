b4fea9d5be27996b68e519899b290b1799dd5d96
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-09-02 17:33:44 +0200

    revised variables names; keep stem lineage

--- R/convert_to_newick.R
+++ R/convert_to_newick.R
@@ -11,42 +11,43 @@
 #' @export
 
 convert_to_newick <- function(phylo_tbl) {
-  # Stupid but necessary for the build
-  time_birth <- NULL
 
-  newick_tbl <- phylo_tbl %>% dplyr::arrange(time_birth)
+  # species must be ordered by chronological order
+  newick_tbl <- phylo_tbl[order(phylo_tbl$time_birth), ]
 
+  # Concatenate Newick string recursively
   while (nrow(newick_tbl) > 1) {
 
-    tip_row <- max(
-      which.max(newick_tbl$time_birth),
-      2 # dirty hack to exclude the origin
+    # Process tips from youngest to oldest
+    child <- which.max(newick_tbl$time_birth)
+    child <- max(child, 2) # skip the MRCA
+    child_name <- newick_tbl$species_name[child]
+
+    parent_name <- newick_tbl$ancestor_name[child]
+
+    # Look for tip/clade containing the parent
+    parent <- grep(
+      pattern = parent_name,
+      x = newick_tbl$species_name
     )
-    child_name <- newick_tbl$species_name[tip_row]
-    parent_name <- newick_tbl$ancestor_name[tip_row]
-    parent_row <- grep(pattern = parent_name, x = newick_tbl$species_name)
-    if (length(parent_row) > 1) {
-      stop(
-        "A string in the 'ancestor_name' column was matched by multiple rows."
-      )
-    }
+
     string_parent <- paste0(
-      newick_tbl$species_name[parent_row], ":",
-      newick_tbl$time_death[parent_row] - newick_tbl$time_birth[tip_row]
+      newick_tbl$species_name[parent], ":",
+      newick_tbl$time_death[parent] - newick_tbl$time_birth[child]
     )
     string_child <- paste0(
       child_name, ":",
-      newick_tbl$time_death[tip_row] - newick_tbl$time_birth[tip_row]
+      newick_tbl$time_death[child] - newick_tbl$time_birth[child]
     )
     newick_string <- paste0("(", string_parent, ",", string_child, ")")
 
-    newick_tbl$species_name[parent_row] <- newick_string
-    newick_tbl$time_death[parent_row] <- newick_tbl$time_birth[tip_row]
-    newick_tbl <- newick_tbl[-tip_row, ]
+    newick_tbl$species_name[parent] <- newick_string
+    newick_tbl$time_death[parent] <- newick_tbl$time_birth[child]
+    newick_tbl <- newick_tbl[-child, ]
   }
 
   newick_string <- paste0(
-    newick_tbl$species_name, ":", newick_tbl$time_death, ";"
+    "(", newick_tbl$species_name, ":", newick_tbl$time_death, ");"
   )
   newick_string
 }
