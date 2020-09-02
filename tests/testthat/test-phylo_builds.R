613ad7d4075033572d00b7e579f6e0590455451d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-09-02 17:32:12 +0200

    fix test branch lengths rm internal node labels

--- tests/testthat/test-phylo_builds.R
+++ tests/testthat/test-phylo_builds.R
@@ -21,12 +21,12 @@ testthat::test_that("phylo builds", {
   # sp1 >----------|
   #                |-----------> sp2
 
-  exptd_phylo <- ape::read.tree(text = "((sp1:500, sp2:500)sp1:500);")
+  exptd_phylo <- ape::read.tree(text = "((sp1:500, sp2:500):500);")
   comrad_tbl <- tibble::tibble(
-    "t" = c(1:1000, 501:1000),
+    "t" = c(1:1000, 500:1000),
     "z" = 0,
-    "species" = c(rep("sp1", 1000), rep("sp2", 500)),
-    "ancestral_species" = c(rep(NA, 1000), rep("sp1", 500))
+    "species" = c(rep("sp1", 1000), rep("sp2", 501)),
+    "ancestral_species" = c(rep(NA, 1000), rep("sp1", 501))
   )
   testthat::expect_silent(
     phylo <- comrad_tbl %>% convert_sim_to_phylo()
@@ -40,12 +40,12 @@ testthat::test_that("phylo builds", {
   # sp1 >----------|
   #                |-----------> sp2
   #
-  exptd_phylo <- ape::read.tree(text = "((sp1:250, sp2:500)sp1:500);")
+  exptd_phylo <- ape::read.tree(text = "((sp1:250, sp2:500):500);")
   comrad_tbl <- tibble::tibble(
-    "t" = c(1:750, 501:1000),
+    "t" = c(1:750, 500:1000),
     "z" = 0,
-    "species" = c(rep("sp1", 750), rep("sp2", 500)),
-    "ancestral_species" = c(rep(NA, 750), rep("sp1", 500))
+    "species" = c(rep("sp1", 750), rep("sp2", 501)),
+    "ancestral_species" = c(rep(NA, 750), rep("sp1", 501))
   )
   testthat::expect_silent(
     phylo <- comrad_tbl %>% convert_sim_to_phylo()
@@ -56,7 +56,7 @@ testthat::test_that("phylo builds", {
 })
 
 testthat::test_that("trait distribution shouldn't matter", {
-  if (Sys.getenv("TRAVIS") != "") {
+  if (Sys.getenv("TRAVIS") == "") {
     skip("only run on CI")
   } else {
 
@@ -71,25 +71,25 @@ testthat::test_that("trait distribution shouldn't matter", {
     # with 50 inds/sp/gen & randomly distributed traits
 
     exptd_phylo <- ape::read.tree(
-      text = "(((sp1:500,sp3:500)sp1:250,(sp2:500,sp4:250)sp2:250)sp1:500);"
+      text = "(((sp1:500,sp3:500):250,(sp2:500,sp4:250):250):250);"
     )
     gen_seq <- c(
       rep(1:1000, rep(50, 1000)),   # sp1
-      rep(251:1000, rep(50, 750)),  # sp2
-      rep(501:1000, rep(50, 500)),  # sp3
-      rep(501:750, rep(50, 250))    # sp4
+      rep(250:1000, rep(50, 751)),  # sp2
+      rep(500:1000, rep(50, 501)),  # sp3
+      rep(500:750, rep(50, 251))    # sp4
     )
     spp_seq <- c(
       rep("sp1", 50000),
-      rep("sp2", 37500),
-      rep("sp3", 25000),
-      rep("sp4", 12500)
+      rep("sp2", 37550),
+      rep("sp3", 25050),
+      rep("sp4", 12550)
     )
     anc_spp_seq <- c(
       rep(as.character(NA), 50000),
-      rep("sp1", 37500),
-      rep("sp1", 25000),
-      rep("sp2", 12500)
+      rep("sp1", 37550),
+      rep("sp1", 25050),
+      rep("sp2", 12550)
     )
 
     comrad_tbl <- tibble::tibble(
@@ -105,14 +105,6 @@ testthat::test_that("trait distribution shouldn't matter", {
 
         # Draw random values for traits
         comrad_tbl$z <- rnorm(length(gen_seq), 0, 1)
-        comrad_tbl %>%
-          dplyr::bind_rows(tibble::tibble(
-            "t" = 0,
-            "z" = rnorm(10, 0, 1), # values drawn below
-            "species" = "sp1",
-            "ancestral_species" = NA
-          )) %>%
-          plot_comm_trait_evolution(hex_fill = "counts")
 
         testthat::expect_silent(
           phylo <- comrad_tbl %>% convert_sim_to_phylo()
