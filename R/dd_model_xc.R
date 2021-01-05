4703c7f3e787b8e73d768740e99c13d68e4740f2
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-01-05 10:38:27 +0100

    5 new DD models, combinations of exponential and linear dependence on speciation / extinction rates

---
+++ R/dd_model_xc.R
@@ -0,0 +1,35 @@
+#' Diversity-dependent model with exponential (x) dependence on speciation and
+#' constant (c) extinction
+#'
+#' A list specifying a DD model with exponential diversity-dependence on the
+#' speciation rate and constant-rate extinction; to be fed as argument
+#' `dd_model` to [comrad::fit_dd_model()].
+#'
+#' \deqn{\lambda(N) = \lambda_{0}(\frac{\mu_{0}}{\lambda_{0}})^{\frac{N}{K}} \\ \mu(N) = \mu_{0}}
+#'
+#' @author Theo Pannetier
+#' @export
+dd_model_xc <- function() {
+  list(
+    "speciation_func" = function(params, N) {
+      params["lambda_0"] * (params["mu_0"] / params["lambda_0"]) ^ (N / params["k"])
+    },
+    "extinction_func" = function(params, N) {
+      params["mu_0"]
+    },
+    "constraints" = list(
+      function(params, ...) params["lambda_0"] > 0,
+      function(params, ...) params["mu_0"] > 0,
+      function(params, ...) params["k"] > 0,
+      function(params, ...) params["lambda_0"] > params["mu_0"]
+    ),
+    "params_check" = function(params) {
+      params_names <- names(params)
+      if (!(length(params_names) == 3 &&
+            all(params_names %in% c("lambda_0", "mu_0", "k"))
+      )) {
+        stop("params for ddmodel_xc should be \"lambda_0\", \"mu_0\" and \"k\".")
+      }
+    }
+  )
+}
