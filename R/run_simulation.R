a5e657ae0ac65c1531669b46e7c0bdad14bda505
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-02-09 10:34:54 +0100

    Sample community on speciation/extinction rather than set freq

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -13,6 +13,9 @@
 #' initial community was taken from (a filename or anything else).
 #' @param nb_gens integer, the number of generations to run the
 #' simulation for.
+#' @param sampling_on_event logical. If `TRUE`, the community is sampled every
+#' time a speciation or extinction happens, and `sampling_freq` is ignored and
+#' must be set to `NA`.
 #' @param sampling_freq numeric \code{> 0}, the frequency (in generations) at
 #' which the community is written to output. See [set_sampling_freq()] for the
 #' default option.
@@ -51,7 +54,10 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
   prob_mutation = comrad::default_prob_mutation(),
   mutation_sd = comrad::default_mutation_sd(),
   trait_dist_sp = comrad::default_trait_dist_sp(),
-  sampling_freq = comrad::set_sampling_freq(nb_gens),
+  sampling_on_event = FALSE,
+  sampling_freq = ifelse(
+    sampling_on_event, NA, comrad::set_sampling_freq(nb_gens)
+  ),
   sampling_frac = comrad::default_sampling_frac(),
   seed = comrad::default_seed(),
   hpc_job_id = NULL
@@ -77,8 +83,15 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
   comrad::testarg_pos(nb_gens)
   comrad::testarg_not_this(nb_gens, c(0, Inf))
   comrad::testarg_int(nb_gens)
-  comrad::testarg_num(sampling_freq)
-  comrad::testarg_int(sampling_freq)
+  comrad::testarg_log(sampling_on_event)
+  if (sampling_on_event) {
+    if (!is.na(sampling_freq)) {
+      stop("If \"sampling_on_event\" is TRUE \"sampling_freq\" must be NA.")
+    }
+  } else {
+    comrad::testarg_num(sampling_freq)
+    comrad::testarg_int(sampling_freq)
+  }
   comrad::testarg_num(seed)
   comrad::testarg_int(seed)
   comrad::testarg_num(growth_rate)
@@ -161,6 +174,8 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
   # Go :)
   time_seq <- (first_gen + 1):(first_gen + nb_gens)
   for (t in time_seq) {
+
+    species_before <- unique(comrad_tbl$species)
     # Replace comm with next generation
     comrad_tbl <- dplyr::bind_cols(
       # Time [t] # nolint
@@ -178,6 +193,8 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
         trait_dist_sp = trait_dist_sp
       )
     )
+    species_after <- unique(comrad_tbl$species)
+
     if (nrow(comrad_tbl) < 1) {
       cat("\nCommunity has gone extinct at generation", t, "\n")
       if (is.null(path_to_output)) {
@@ -187,8 +204,13 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
       }
     }
 
-    if (t %% sampling_freq == 0) {
-      cat("\nRunning generation", t, "/", nb_gens)
+    if (sampling_on_event) {
+      sample_this_gen <- setequal(spp_before, spp_after)
+    } else {
+      sample_this_gen <- t %% sampling_freq == 0
+    }
+
+    if (sample_this_gen) {
       if (!is.null(path_to_output)) {
         # Write only a sample of the output
         sampled_output <- comrad::sample_output(
@@ -201,6 +223,7 @@ run_simulation <- function( # nolint, ignore high cyclomatic complexity
           append = TRUE
         )
       }
+      cat("\nSampled generation", t, "/", nb_gens)
     }
   }
 
