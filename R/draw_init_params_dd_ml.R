fffd6a4853f89bd65c7715cf2d09e75679f19b9e
Theo Pannetier <theo@pannetier.nl>
2024-12-23 14:39:54 +0100

    fix k values sometimes being sampled too small

--- R/draw_init_params_dd_ml.R
+++ R/draw_init_params_dd_ml.R
@@ -25,6 +25,8 @@
 #' @export
 draw_init_params_dd_ml <- function(nb_sets, phylos, dd_model) {
 
+  with_alpha <- !stringr::str_detect(dd_model$name, "c") # no alpha if constant
+
   ltt_tbl <- comrad::get_ltt_tbl(phylos[[1]]) %>%
     dplyr::mutate("time" = time - min(time))
   n_max <- phylos %>%
@@ -37,8 +39,10 @@ draw_init_params_dd_ml <- function(nb_sets, phylos, dd_model) {
   # Initial parameter values
   lambdas <- proto_lambda0 * 10 ^ stats::runif(nb_sets, -3, 2)
   mus <- stats::runif(nb_sets, 0, 0.75 * lambdas)
-  ks <- ceiling(n_max * 5 ^ stats::runif(nb_sets, -1, 1))
-  alphas <- stats::runif(nb_sets, 0, 1)
+  alphas <- ifelse(with_alpha, stats::runif(nb_sets, 0, 1), rep(0, nb_sets))
+  # ensure k prime is not smaller than n_max
+  min_ks <- n_max * (lambdas - mus) / (1 - alphas) / lambdas
+  ks <- pmax(min_ks, ceiling(n_max * 5 ^ stats::runif(nb_sets, -1, 1)))
 
   init_params <- purrr::pmap(
     list(lambdas, mus, ks, alphas),
@@ -46,7 +50,6 @@ draw_init_params_dd_ml <- function(nb_sets, phylos, dd_model) {
       c("lambda_0" = lambda, "mu_0" = mu, "k" = k, "alpha" = alpha)
     })
 
-  with_alpha <- !stringr::str_detect(dd_model$name, "c") # no alpha if constant
 
   if (!with_alpha) {
     init_params <- init_params %>% purrr::map(function(vec) vec[-4])
